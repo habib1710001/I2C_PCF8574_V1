68c424c3e83acb6754fddaa67c4018ab2a14bca8
HABIB UR RAHMAN <78023625+habib1710001@users.noreply.github.com>
2023-06-07 15:55:17 +0600

    Initial commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/core_cm4.h \
+ E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/cmsis_version.h \
+ E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/cmsis_compiler.h \
+ E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/cmsis_gcc.h \
+ E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/mpu_armv7.h \
+ E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/core_cm4.h:
+E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/cmsis_version.h:
+E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/cmsis_compiler.h:
+E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/cmsis_gcc.h:
+E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Include/mpu_armv7.h:
+E:/Embedded_System/Microcontrolling_Unit/Bare-Metal_Programming_ARM_Cortex-M/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ README.md
@@ -0,0 +1,2 @@
+# I2C_MPU6050_V1
+ I2C Library for the MPU6050
