ccfbef87154db6a4dca40583a4314cb78f4b2849
HABIB UR RAHMAN <78023625+habib1710001@users.noreply.github.com>
2023-06-10 15:54:35 +0600

    I2C Start Stop Write and WriteMulti functions added

--- Src/main.c
+++ Src/main.c
@@ -1,9 +1,25 @@
 #include "stm32f4xx.h"
 #include "i2c.h"
 
+
+
+
+
+
+
 int main(void){
 
+	//Generate Start Bit
+    I2C1_Start();
 
+    //Send the slave address
+    I2C_Address(0x4E);
+
+    //Write the data
+    I2C_Write(0x01);
+
+    //Generate stop bit
+    I2C_Stop();
 	while(1)
 	{
 
